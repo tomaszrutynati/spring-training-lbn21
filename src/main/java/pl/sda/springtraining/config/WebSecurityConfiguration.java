38a05cdae2decbc0d885d033f87ffc2d2f98713a
Tomasz.Rutyna <Poniatowa2021!>
2020-09-06 12:38:45 +0200

    spring - security - in memory users

---
+++ src/main/java/pl/sda/springtraining/config/WebSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package pl.sda.springtraining.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("test")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("test")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/patient", "/doctor").authenticated()
+            .and().authorizeRequests().antMatchers("/").permitAll()
+            .and().authorizeRequests().antMatchers("/patient/**", "/doctor/**")
+                .hasRole("ADMIN")
+            .and().formLogin();
+    }
+}
