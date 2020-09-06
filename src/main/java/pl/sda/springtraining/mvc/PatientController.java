7cf2044fda4a36d62995a6c1580267736615970e
Tomasz.Rutyna <Poniatowa2021!>
2020-09-06 10:02:10 +0200

    spring - thymleaf - form

--- src/main/java/pl/sda/springtraining/mvc/PatientController.java
+++ src/main/java/pl/sda/springtraining/mvc/PatientController.java
@@ -2,9 +2,10 @@ package pl.sda.springtraining.mvc;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import pl.sda.springtraining.domain.patient.Patient;
 import pl.sda.springtraining.domain.patient.PatientService;
 
 @Controller
@@ -14,10 +15,31 @@ public class PatientController {
 
     private final PatientService patientService;
 
-    @GetMapping
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.GET})
     ModelAndView allPatientsPage() {
         ModelAndView mav = new ModelAndView("patients.html");
         mav.addObject("patients", patientService.getAll());
         return mav;
     }
+
+    @GetMapping("/add")
+    ModelAndView addPatientPage() {
+        ModelAndView mav = new ModelAndView("addPatient.html");
+        mav.addObject("patient", new Patient());
+        return mav;
+    }
+
+    @PostMapping("/add")
+    String addNewPatient(@ModelAttribute("patient") Patient newPatient) {
+        patientService.create(newPatient);
+
+        return "redirect:/patient";
+    }
+
+    /* Drugi sposob na metode wyciagajaca dane z formularza
+    @PostMapping("/add")
+    String addNewPatient(Model model) {
+        Patient newPatient = (Patient) model.getAttribute("patient");
+    }
+     */
 }
